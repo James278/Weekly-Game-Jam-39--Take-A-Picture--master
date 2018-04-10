b192d7acc29c8a852e0d4484445812f5f6169a8b
DESKTOP-3GOBICS\User <jamesperry27@hotmail.co.uk>
2018-04-10 21:12:34 +0100

    Paintings added, Enemy Spawning, Exit goal visible

---
+++ Take_a_Picture-master/Assets/Scripts/Picture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picture : MonoBehaviour {
+    ScoreKeeper scoreKeeper;
+
+    [SerializeField] GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Instantiate(prefab, new Vector3(26f, 0, 10f), Quaternion.identity);
+            Destroy(this.gameObject);
+            scoreKeeper.Score(1);
+        }
+    }
+}
